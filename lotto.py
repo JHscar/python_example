2855d21c865aa8b967ace63a67155646465a1eef
Lee Jae Hun <wogns5206@naver.com>
2019-08-30 15:41:42 +0900

    python example3

---
+++ lotto.py
@@ -0,0 +1,15 @@
+import random
+import requests
+
+lotto = sorted(random.sample(range(1, 46), 6))
+
+url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=873'
+res = requests.get(url)
+data = res.json()
+winner = []
+
+for i in range(1, 7):
+    winner.append(data[f'drwtNo{i}'])
+
+match = len(set(lotto) & set(winner))
+print(match)
