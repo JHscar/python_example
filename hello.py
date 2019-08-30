4eb7dce566117680a73758b09a7145702e6322e5
Lee Jae Hun <wogns5206@naver.com>
2019-08-30 15:40:32 +0900

    python example

---
+++ hello.py
@@ -0,0 +1,29 @@
+name = "Scar"
+age = 35
+
+if age > 40 :
+    print('ㅇㅏ재')
+else :
+    print('아 재 아 님')
+
+# 반복
+# 1. 단순반복 -> (while, for)
+# 2. 순회 -> (forEach) : 특정 자료를 한 번씩만 조회
+# - 배열, 오브젝트
+
+foods = ['바스버거', '브록시', '고갯마루', '대우식당', '베이징코야', '백운봉 막국수', '돝고기', '경천애인']
+print(foods[-1])
+
+for e in foods:
+    print(e)
+
+# if 조건:
+#     참일때 할 것
+# 이건 single-line
+
+
+"""
+이건 multi-line
+여기는 멀티라인 커멘트
+여러줄을 쓸 수 있어요
+"""
